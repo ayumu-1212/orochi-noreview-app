2e55295ab7f3f9972ab65721a64c109d489a7293
Yuto Tachibana <yuto.tachibana@dena.com>
2023-12-12 16:36:23 +0900

    クソテスト

---
+++ src/utils/chance/chance.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, it } from 'vitest'
+
+import { chance } from './'
+
+describe('本当に正しい確率になっているのか、実際に実行することで真心のこもったテストを書く', () => {
+  it('10%', () => {
+    const TIMES = 10_000_000
+    let results = []
+    for (let i = -0; i < TIMES; i++) {
+      results.push(chance(10))
+    }
+    // TIMES回実行したうち何パーセントがtrueだったか
+    const result = (results.filter((v) => v).length / TIMES) * 100
+    expect(result).toBeGreaterThan(9.99)
+    expect(result).toBeLessThan(10.01)
+  })
+})
