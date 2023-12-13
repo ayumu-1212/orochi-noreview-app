ed82725c2d9e945399253b86ad76dc2c4e709031
ayoshida1101 <aukt1011161@icloud.com>
2023-12-13 21:12:14 +0900

    loading の表示を実装

--- src/app/(bg)/rsa/page.tsx
+++ src/app/(bg)/rsa/page.tsx
@@ -110,151 +110,162 @@ export default function Home() {
     <PythonProvider>
       <main>
         <div className={boxStyle}>
-          <h1 className={h1Style}>RSA暗号を作ってみよう</h1>
-          <h2 className={h2Style}>公開鍵と秘密鍵を作る</h2>
-          <section className={sectionStyle}>
-            <h3 className={h3Style}>互いに素なPとQを入力してください</h3>
-            <p className={pStyle}>
-              <span>P = </span>
-              <input
-                type="number"
-                value={p}
-                className={inputNumberStyle}
-                onChange={(e) => setP(Number(e.target.value))}
-              />
-              <span>, Q = </span>
-              <input
-                type="number"
-                value={q}
-                className={inputNumberStyle}
-                onChange={(e) => setQ(Number(e.target.value))}
-              />
-            </p>
-            <h3 className={h3Style}>N = P * Q を行います</h3>
-            <p className={pStyle}>
-              <span>N = </span>
-              <span>{n || '?'} </span>
-              <button onClick={() => calcN(p, q)} className={buttonStyle}>
-                Nを計算
-              </button>
-            </p>
-            <h3 className={h3Style}>L = P-1とQ-1の最小公倍数 になります</h3>
-            <p className={pStyle}>
-              <span>L = </span>
-              <span>{l || '?'} </span>
-              <button onClick={() => calcL(p, q)} className={buttonStyle}>
-                Lを計算
-              </button>
-            </p>
-            <h3 className={h3Style}>
-              EとLの最大公約数が1となるようなEを選びます
-            </h3>
-            <p className={pStyle}>
-              <button onClick={() => proposalE(l)} className={buttonStyle}>
-                Eの候補を出す
-              </button>
-            </p>
-            <p className={pStyle}>
-              <span>Eの候補: </span>
-              <span>{es.join(', ')}</span>
-            </p>
-            <p className={pStyle}>
-              <span>E = </span>
-              <select
-                value={e}
-                className={inputSelectStyle}
-                onChange={(e) => setE(Number(e.target.value))}
-              >
-                <option key={0} value={0}>
-                  Eを選ぶ
-                </option>
-                {es.map((e) => (
-                  <option key={e} value={e}>
-                    {e}
-                  </option>
-                ))}
-              </select>
-            </p>
-            <h3 className={h3Style}>D * E % L が 1 となるようなDを選びます</h3>
-            <p className={pStyle}>
-              <button onClick={() => proposalD(e, l)} className={buttonStyle}>
-                Dの候補を出す
-              </button>
-            </p>
-            <p className={pStyle}>
-              <span>Dの候補: </span>
-              <span>{ds.join(', ')}</span>
-            </p>
-            <p className={pStyle}>
-              <span>D = </span>
-              <select
-                value={d}
-                className={inputSelectStyle}
-                onChange={(e) => setD(Number(e.target.value))}
-              >
-                <option key={0} value={0}>
-                  Dを選ぶ
-                </option>
-                {ds.map((d) => (
-                  <option key={d} value={d}>
-                    {d}
-                  </option>
-                ))}
-              </select>
-            </p>
-            <h3 className={h3Style}>
-              上記で作った E と D と N の組み合わせが鍵となります
-            </h3>
-            <p className={pStyle}>{`公開鍵 = {E, N} = {${e}, ${n}}`}</p>
-            <p className={pStyle}>{`秘密鍵 = {D, N} = {${d}, ${n}}`}</p>
-          </section>
-          <h2 className={h2Style}>暗号化する</h2>
-          <section className={sectionStyle}>
-            <h3 className={h3Style}>暗号化して送りたい数字を入力します</h3>
-            <p className={pStyle}>
-              <span>送りたい数字 = </span>
-              <input
-                type="number"
-                value={send}
-                className={inputNumberStyle}
-                onChange={(e) => setSend(Number(e.target.value))}
-              />
-            </p>
-            <h3 className={h3Style}>
-              「送りたい数字^E mod N 」が暗号化された数字になります
-            </h3>
-            <p className={pStyle}>
-              <span>暗号文 = </span>
-              <span>{encry} </span>
-              <button
-                onClick={() => encrypt(send, e, n)}
-                className={buttonStyle}
-              >
-                暗号化
-              </button>
-            </p>
-          </section>
-          <h2 className={h2Style}>復号化する</h2>
-          <section className={sectionStyle}>
-            <h3 className={h3Style}>
-              「暗号文^D mod N」が復号化された数字になります
-            </h3>
-            <p className={pStyle}>
-              <span>復号化した数字 = </span>
-              <span>{decry} </span>
-              <button
-                onClick={() => decrypt(encry, d, n)}
-                className={buttonStyle}
-              >
-                復号化
-              </button>
-            </p>
-          </section>
-          <div className={linkWrapperStyle}>
-            <a href="../" className={linkStyle}>
-              トップへ
-            </a>
-          </div>
+          {isLoading ? (
+            <h1 className={h1Style}>ちょっと待ってね</h1>
+          ) : (
+            <>
+              <h1 className={h1Style}>RSA暗号を作ってみよう</h1>
+              <h2 className={h2Style}>公開鍵と秘密鍵を作る</h2>
+              <section className={sectionStyle}>
+                <h3 className={h3Style}>互いに素なPとQを入力してください</h3>
+                <p className={pStyle}>
+                  <span>P = </span>
+                  <input
+                    type="number"
+                    value={p}
+                    className={inputNumberStyle}
+                    onChange={(e) => setP(Number(e.target.value))}
+                  />
+                  <span>, Q = </span>
+                  <input
+                    type="number"
+                    value={q}
+                    className={inputNumberStyle}
+                    onChange={(e) => setQ(Number(e.target.value))}
+                  />
+                </p>
+                <h3 className={h3Style}>N = P * Q を行います</h3>
+                <p className={pStyle}>
+                  <span>N = </span>
+                  <span>{n || '?'} </span>
+                  <button onClick={() => calcN(p, q)} className={buttonStyle}>
+                    Nを計算
+                  </button>
+                </p>
+                <h3 className={h3Style}>L = P-1とQ-1の最小公倍数 になります</h3>
+                <p className={pStyle}>
+                  <span>L = </span>
+                  <span>{l || '?'} </span>
+                  <button onClick={() => calcL(p, q)} className={buttonStyle}>
+                    Lを計算
+                  </button>
+                </p>
+                <h3 className={h3Style}>
+                  EとLの最大公約数が1となるようなEを選びます
+                </h3>
+                <p className={pStyle}>
+                  <button onClick={() => proposalE(l)} className={buttonStyle}>
+                    Eの候補を出す
+                  </button>
+                </p>
+                <p className={pStyle}>
+                  <span>Eの候補: </span>
+                  <span>{es.join(', ')}</span>
+                </p>
+                <p className={pStyle}>
+                  <span>E = </span>
+                  <select
+                    value={e}
+                    className={inputSelectStyle}
+                    onChange={(e) => setE(Number(e.target.value))}
+                  >
+                    <option key={0} value={0}>
+                      Eを選ぶ
+                    </option>
+                    {es.map((e) => (
+                      <option key={e} value={e}>
+                        {e}
+                      </option>
+                    ))}
+                  </select>
+                </p>
+                <h3 className={h3Style}>
+                  D * E % L が 1 となるようなDを選びます
+                </h3>
+                <p className={pStyle}>
+                  <button
+                    onClick={() => proposalD(e, l)}
+                    className={buttonStyle}
+                  >
+                    Dの候補を出す
+                  </button>
+                </p>
+                <p className={pStyle}>
+                  <span>Dの候補: </span>
+                  <span>{ds.join(', ')}</span>
+                </p>
+                <p className={pStyle}>
+                  <span>D = </span>
+                  <select
+                    value={d}
+                    className={inputSelectStyle}
+                    onChange={(e) => setD(Number(e.target.value))}
+                  >
+                    <option key={0} value={0}>
+                      Dを選ぶ
+                    </option>
+                    {ds.map((d) => (
+                      <option key={d} value={d}>
+                        {d}
+                      </option>
+                    ))}
+                  </select>
+                </p>
+                <h3 className={h3Style}>
+                  上記で作った E と D と N の組み合わせが鍵となります
+                </h3>
+                <p className={pStyle}>{`公開鍵 = {E, N} = {${e}, ${n}}`}</p>
+                <p className={pStyle}>{`秘密鍵 = {D, N} = {${d}, ${n}}`}</p>
+              </section>
+              <h2 className={h2Style}>暗号化する</h2>
+              <section className={sectionStyle}>
+                <h3 className={h3Style}>暗号化して送りたい数字を入力します</h3>
+                <p className={pStyle}>
+                  <span>送りたい数字 = </span>
+                  <input
+                    type="number"
+                    value={send}
+                    className={inputNumberStyle}
+                    onChange={(e) => setSend(Number(e.target.value))}
+                  />
+                </p>
+                <h3 className={h3Style}>
+                  「送りたい数字^E mod N 」が暗号化された数字になります
+                </h3>
+                <p className={pStyle}>
+                  <span>暗号文 = </span>
+                  <span>{encry} </span>
+                  <button
+                    onClick={() => encrypt(send, e, n)}
+                    className={buttonStyle}
+                  >
+                    暗号化
+                  </button>
+                </p>
+              </section>
+              <h2 className={h2Style}>復号化する</h2>
+              <section className={sectionStyle}>
+                <h3 className={h3Style}>
+                  「暗号文^D mod N」が復号化された数字になります
+                </h3>
+                <p className={pStyle}>
+                  <span>復号化した数字 = </span>
+                  <span>{decry} </span>
+                  <button
+                    onClick={() => decrypt(encry, d, n)}
+                    className={buttonStyle}
+                  >
+                    復号化
+                  </button>
+                </p>
+              </section>
+              <div className={linkWrapperStyle}>
+                <a href="../" className={linkStyle}>
+                  トップへ
+                </a>
+              </div>
+            </>
+          )}
         </div>
       </main>
     </PythonProvider>
